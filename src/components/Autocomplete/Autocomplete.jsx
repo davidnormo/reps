898b0593c147f3c0584b9ab7a7a53514f9602b6b
Dave Normington <dave.normington@troweprice.com>
2023-07-18 17:52:12 +0100

    autocomplete styling

--- src/components/Autocomplete/Autocomplete.jsx
+++ src/components/Autocomplete/Autocomplete.jsx
@@ -19,9 +19,13 @@ export default function Autocomplete({ placeholder, getResults, onCommit }) {
         onChange={(query) => setQ(query)}
       />
       {!!results.length && (
-        <div className="border p-3 rounded-b-lg pt-4 -mt-4 border-t-0">
+        <div className="border p-3 pb-0 pt-1 rounded-b-lg pt-4 -mt-4 border-t-0">
           {results.map((result) => (
-            <div key={result.id} onClick={() => onCommit(result)}>
+            <div
+              className="p-3 border-b last:border-b-0"
+              key={result.id}
+              onClick={() => onCommit(result)}
+            >
               {result.label}
             </div>
           ))}
