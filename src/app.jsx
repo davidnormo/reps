440a2577399af2072ede00d12c94092b594bc588
Dave Normington <dave.normington@troweprice.com>
2023-07-18 10:53:01 +0100

    eslint + prettier

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,17 @@
-import './app.css'
-import { Nav } from './components/nav/nav'
-import useNav from './hooks/useNav';
+import "./app.css";
+import { Nav } from "./components/nav/nav";
+import useNav from "./hooks/useNav";
 
 export function App() {
   const { page } = useNav();
 
   return (
     <div className="h-full">
-      <h1 className="text-3xl font-bold mt-1 ml-1">
-        Reps
-      </h1>
+      <h1 className="text-3xl font-bold mt-1 ml-1">Reps</h1>
 
       <h2>{page}</h2>
-      
+
       <Nav />
     </div>
-  )
+  );
 }
