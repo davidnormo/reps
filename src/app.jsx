c0683bf5a867e07139acea0c0c18dfe713f7bc75
Dave Normington <dave.normington@troweprice.com>
2023-07-19 11:41:49 +0100

    favicon changes and logo

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import "./app.css";
 import { Nav } from "./components/nav/nav";
 import { DataCtx, useSetupData } from "./hooks/useData";
 import { NavCtx, useNavSetup } from "./hooks/useNav";
+import logo from "/android-chrome-512x512.png";
 
 export function App() {
   const { PageComponent, ...navValue } = useNavSetup();
@@ -12,7 +13,10 @@ export function App() {
     <NavCtx.Provider value={navValue}>
       <DataCtx.Provider value={dataValue}>
         <div className="w-full h-screen flex flex-col overflow-hidden">
-          <h1 className="text-3xl font-bold mt-1 ml-1">Reps</h1>
+          <div className="flex flex-row justify-start items-center px-3 py-3">
+            <img src={logo} className="shrink max-h-[32px] max-w-[32px]" />
+            <h1 className="text-xl font-bold ml-2">Reps</h1>
+          </div>
           <div
             className="overflow-scroll"
             style={{ height: "calc(100vh - 36px - 51px)" }}
--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/reps/android-chrome-192.png",
+            src: "/reps/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/reps/android-chrome-512.png",
+            src: "/reps/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
